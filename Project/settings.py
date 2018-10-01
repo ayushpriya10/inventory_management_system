091314593d607552446c66c16ffd1777b4c6638f
Ayush Priya <ayushpriya10@gmail.com>
2018-10-01 23:38:36 +0530

    Configuring App for deployment.

--- Project/settings.py
+++ Project/settings.py
@@ -134,3 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
